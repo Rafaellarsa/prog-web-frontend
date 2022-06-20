6bc98bc7f755943ccb0068086b34bb00f40cd408
Rafaella Sampaio <sampaiorafaella@hotmail.com>
2022-06-20 01:19:26 -0300

    Listar categorias

--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -13,8 +13,8 @@
           <v-icon aria-label="Mais" color="primary">mdi-plus</v-icon>
           Novo produto
         </v-btn>
-        <v-list v-if="productList.length" two-line>
-          <v-list-item v-for="product in productList" :key="product.id">
+        <v-list v-if="products.length" two-line>
+          <v-list-item v-for="product in products" :key="product.id">
             <v-list-item-avatar>
               <img :src="product.foto" :alt="product.nome" />
             </v-list-item-avatar>
@@ -48,16 +48,17 @@
           </v-list-item>
         </v-list>
       </v-tab-item>
+
       <v-tab-item>
         <v-btn text @click="isNewCategoryDialogVisible = true">
           <v-icon aria-label="Mais" color="primary">mdi-plus</v-icon>
           Nova categoria
         </v-btn>
-        <v-list>
+        <v-list v-if="categories.length">
           <v-list-item v-for="category in categories" :key="category.id">
             <v-list-item-content>
               <v-list-item-title>
-                {{ category.name }}
+                {{ category.descricaoCategoria }}
               </v-list-item-title>
             </v-list-item-content>
 
@@ -72,12 +73,14 @@
           </v-list-item>
         </v-list>
       </v-tab-item>
+
       <v-tab-item>
         <v-btn text>
           <v-icon aria-label="Mais" color="primary">mdi-plus</v-icon>
           Novo usuário
         </v-btn>
       </v-tab-item>
+
       <v-tab-item> Visualizar relatórios gerenciais </v-tab-item>
     </v-tabs-items>
 
@@ -98,8 +101,6 @@ import axios from "axios";
 import NewProductDialog from "./Dialogs/NewProductDialog.vue";
 import NewCategoryDialog from "./Dialogs/NewCategoryDialog.vue";
 
-import categories from "@/placeholders/categories.json";
-
 export default {
   name: "AdminPage",
   components: {
@@ -110,15 +111,29 @@ export default {
     tab: null,
     isNewProductDialogVisible: false,
     isNewCategoryDialogVisible: false,
-    categories: categories.categories,
-    productList: [],
+    categories: [],
+    products: [],
   }),
+  watch: {
+    tab(newValue) {
+      console.log(newValue);
+      if (newValue == 1 && !this.categories.length) {
+        axios
+          .get("http://localhost:8080/e-commerce/ListarCategorias")
+          .then((response) => {
+            console.log(response.data);
+            this.categories = response.data;
+          })
+          .catch((error) => console.log(error));
+      }
+    },
+  },
   mounted() {
     axios
       .get("http://localhost:8080/e-commerce/ListarProdutos")
       .then((response) => {
         console.log(response.data);
-        this.productList = response.data;
+        this.products = response.data;
       })
       .catch((error) => console.log(error));
   },
